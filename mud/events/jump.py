7c2fad4a1bd21d039f1df49b40e777409e333394
Mathis Dupuis <mathis.dupuis@etu.univ-orleans.fr>
2020-05-08 15:38:26 +0200

    enfin le jump marche git add ./mud/games/nain/initial.yml !

--- mud/events/jump.py
+++ mud/events/jump.py
@@ -2,7 +2,10 @@ from .event import Event1
 
 class JumpEvent(Event1):
     NAME = "jump"
-
+    
+    def get_event_templates(self):
+        return self.actor.container().get_event_templates()
+        
     def perform(self):
         loc = self.actor.container()
         if not loc.has_prop("weak"):
