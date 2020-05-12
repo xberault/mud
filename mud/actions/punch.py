8a32e896459649cb9dee5377a7ea342590ccc1aa
Tristan Bellot <bellot.tristan@gmail.com>
2020-05-12 18:23:15 +0200

    ajout salle5 et koala

--- mud/actions/punch.py
+++ mud/actions/punch.py
@@ -1,7 +1,7 @@
 from .action import Action3
 from mud.events import PunchWithEvent
 
-class BrokeAction(Action3):
+class PunchWithAction(Action3):
     EVENT = PunchWithEvent
     RESOLVE_OBJECT = "resolve_for_operate"
     RESOLVE_OBJECT2 = "resolve_for_use"
