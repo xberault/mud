a24523e25ec718d8dfbf07bae3e296ff23262d69
Xavier Berault <xavier.berault@etu.univ-orleans.fr>
2020-05-13 15:03:26 +0200

    ajout description punchWithAction

--- mud/games/nain/__init__.py
+++ mud/games/nain/__init__.py
@@ -8,6 +8,7 @@ from mud.actions import (
     LightOnAction, LightOffAction, DropAction, DropInAction,
     PushAction, TeleportAction, EnterAction, LeaveAction,
     JumpAction, BarkAction, BrokeAction, DrinkAction,
+    PunchWithAction
 )
 
 import mud.game
@@ -20,25 +21,26 @@ def make_rules():
     DETS = "(?:l |le |la |les |une |un |)"
 
     return (
-        (GoAction       , r"(?:aller |)(%s)" % "|".join(DIRS)),
-        (TakeAction     , r"(?:p|prendre) %s(\S+)" % DETS),
-        (LookAction     , r"(?:r|regarder)"),
-        (InspectAction  , r"(?:r|regarder|lire|inspecter|observer) %s(\S+)" % DETS),
-        (OpenAction     , r"ouvrir %s(\S+)" % DETS),
-        (OpenWithAction , r"ouvrir %s(\S+) avec %s(\w+)" % (DETS,DETS)),
-        (CloseAction    , r"fermer %s(\S+)" % DETS),
-        (TypeAction     , r"(?:taper|[eé]crire) (\S+)$"),
-        (InventoryAction, r"(?:inventaire|inv|i)$"),
-        (LightOnAction  , r"allumer %s(\S+)" % DETS),
-        (LightOffAction , r"[eé]teindre %s(\S+)" % DETS),
-        (DropAction     , r"(?:poser|laisser) %s(\S+)" % DETS),
-        (DropInAction   , r"(?:poser|laisser) %s(\S+) (?:dans |sur |)%s(\S+)" % (DETS,DETS)),
-        (PushAction     , r"(?:appuyer|pousser|presser|toucher)(?: sur|) %s(\S+)" % DETS),
-        (TeleportAction , r"tele(?:porter|) (\S+)"),
-        (EnterAction    , r"entrer"),
-        (LeaveAction    , r"sortir|partir"),
-        (JumpAction     , r"sauter|saut|jump"),
-        (BarkAction     , r"aboyer"),
-        (BrokeAction    , r"casser %s(\S+) avec %s(\w+)" % (DETS,DETS)),
-        (DrinkAction    , r"boire %s(\S+)" % DETS),
+        (GoAction           , r"(?:aller |)(%s)" % "|".join(DIRS)),
+        (TakeAction         , r"(?:p|prendre) %s(\S+)" % DETS),
+        (LookAction         , r"(?:r|regarder)"),
+        (InspectAction      , r"(?:r|regarder|lire|inspecter|observer) %s(\S+)" % DETS),
+        (OpenAction         , r"ouvrir %s(\S+)" % DETS),
+        (OpenWithAction     , r"ouvrir %s(\S+) avec %s(\w+)" % (DETS,DETS)),
+        (CloseAction        , r"fermer %s(\S+)" % DETS),
+        (TypeAction         , r"(?:taper|[eé]crire) (\S+)$"),
+        (InventoryAction    , r"(?:inventaire|inv|i)$"),
+        (LightOnAction      , r"allumer %s(\S+)" % DETS),
+        (LightOffAction     , r"[eé]teindre %s(\S+)" % DETS),
+        (DropAction         , r"(?:poser|laisser) %s(\S+)" % DETS),
+        (DropInAction       , r"(?:poser|laisser) %s(\S+) (?:dans |sur |)%s(\S+)" % (DETS,DETS)),
+        (PushAction         , r"(?:appuyer|pousser|presser|toucher)(?: sur|) %s(\S+)" % DETS),
+        (TeleportAction     , r"tele(?:porter|) (\S+)"),
+        (EnterAction        , r"entrer"),
+        (LeaveAction        , r"sortir|partir"),
+        (JumpAction         , r"sauter|saut|jump"),
+        (BarkAction         , r"aboyer"),
+        (BrokeAction        , r"casser %s(\S+) avec %s(\w+)" % (DETS,DETS)),
+        (DrinkAction        , r"boire %s(\S+)" % DETS),
+        (PunchWithAction    ,r"(?:frapper|casser) %s(\S+) (?:le |ce |)%s(\S+)" % (DETS,DETS))
     )
